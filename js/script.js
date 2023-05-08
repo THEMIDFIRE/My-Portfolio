512dbd3bdb691513f36721764637c8adc0d47594
Mohamed Magdy <midomohamedmagdy@hotmail.com>
2023-05-08 05:02:27 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function () {
+  //Changing nav style after scrolling
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > 90) {
+      $("nav").addClass("wider");
+    } else {
+      $("nav").removeClass("wider");
+    };
+  });
+  
+});
