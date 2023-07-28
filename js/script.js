9614cdac84bb428481e67407fc40ac5adb5f672b
Mohamed Magdy <midomohamedmagdy@hotmail.com>
2023-07-28 16:58:09 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,6 +6,11 @@ $(function () {
     } else {
       $("header").removeClass("wider");
     };
+    if ($(window).scrollTop() > 650) {
+      $("#scroll-to-top").addClass("shown");
+    } else {
+      $("#scroll-to-top").removeClass("shown");
+    };
   });
   
 });
